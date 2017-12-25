88a1d4e123dd155bf01397dc967516da4516e22e
Cellularhacker <wva3cdae@gmail.com>
2017-12-26 00:19:32 +0900

    Added a static directory.

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
     res.send('Hello world!');
 });
 
+// Listening on 20204
 app.listen(20204, function() {
     console.log('Example app listening on port 20204!');
 });
+
+// Using Static Public Libraries.
+app.use(express.static('public'));
