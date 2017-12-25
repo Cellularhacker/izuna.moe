e53118f8b091fe626b7fb9d0005db743ca965cd8
Cellularhacker <wva3cdae@gmail.com>
2017-12-26 00:22:48 +0900

    now root('/') shows index.html

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello world!');
-});
+app.use('/', express.static(__dirname + '/public' + '/index.html'))
 
 // Listening on 20204
 app.listen(20204, function() {
