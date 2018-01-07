d82f290178cc39d00eade871ba5d5c882b80b848
Cellularhacker <wva3cdae@gmail.com>
2018-01-08 01:18:27 +0900

    Fixed Copy to Clipboard Button

--- public/main.js
+++ public/main.js
@@ -1,34 +1,7 @@
-function BTC_Adr_Copy() {
-    var copyText = document.getElementById("BTC");
-    copyText.select();
-    document.execCommand("Copy");
-    alert("BTC Address has been successfully copied.");
-}
-
-function QTUM_Adr_Copy() {
-    var copyText = document.getElementById("QTUM");
-    copyText.select();
-    document.execCommand("Copy");
-    alert("QTUM Address has been successfully copied.");
-}
-
-function ETH_Adr_Copy() {
-    var copyText = document.getElementById("ETH");
-    copyText.select();
-    document.execCommand("Copy");
-    alert("ETH Address has been successfully copied.");
-}
-
-function ETC_Adr_Copy() {
-    var copyText = document.getElementById("ETC");
-    copyText.select();
-    document.execCommand("Copy");
-    alert("ETC Address has been successfully copied.");
-}
-
-function POWR_Adr_Copy() {
-    var copyText = document.getElementById("POWR");
-    copyText.select();
-    document.execCommand("Copy");
-    alert("POWR Address has been successfully copied.");
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
 }
