2006930e2f6035329b082b96084d5610263494b8
Cellularhacker <wva3cdae@gmail.com>
2018-01-08 00:28:09 +0900

    Added a each CSS & JS file.

---
+++ public/main.js
@@ -0,0 +1,34 @@
+function BTC_Adr_Copy() {
+    var copyText = document.getElementById("BTC");
+    copyText.select();
+    document.execCommand("Copy");
+    alert("BTC Address has been successfully copied.");
+}
+
+function QTUM_Adr_Copy() {
+    var copyText = document.getElementById("QTUM");
+    copyText.select();
+    document.execCommand("Copy");
+    alert("QTUM Address has been successfully copied.");
+}
+
+function ETH_Adr_Copy() {
+    var copyText = document.getElementById("ETH");
+    copyText.select();
+    document.execCommand("Copy");
+    alert("ETH Address has been successfully copied.");
+}
+
+function ETC_Adr_Copy() {
+    var copyText = document.getElementById("ETC");
+    copyText.select();
+    document.execCommand("Copy");
+    alert("ETC Address has been successfully copied.");
+}
+
+function POWR_Adr_Copy() {
+    var copyText = document.getElementById("POWR");
+    copyText.select();
+    document.execCommand("Copy");
+    alert("POWR Address has been successfully copied.");
+}
